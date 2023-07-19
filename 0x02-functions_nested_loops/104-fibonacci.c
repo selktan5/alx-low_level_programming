7f836a62bb37938763e667f59b96990cfe0ffd17
selktan5 <sandy.elktan@gmail.com>
2023-07-19 15:15:14 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -33,3 +33,19 @@ int main(void)
 		half2 = fib1_half2 + fib2_half2;
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
+
+		printf("%lu%lu", half1, half2);
+		if (count != 98)
+			printf(", ");
+
+		fib1_half1 = fib2_half1;
+		fib1_half2 = fib2_half2;
+		fib2_half1 = half1;
+		fib2_half2 = half2;
+	}
+	printf("\n");
+	return (0);
+}
