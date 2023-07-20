4b21b6a28e521ec52d2419574146aa46eb7937d7
selktan5 <sandy.elktan@gmail.com>
2023-07-20 06:51:24 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
 #include "main.h"
-/**
-* mul - multiplies two integers
-* @a: first integer
-* @b: second integer
-* Return: addition of a and b
-*/
-int mul(int a, int b)
-{
-	return (a * b);
-}
 
+/**
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
